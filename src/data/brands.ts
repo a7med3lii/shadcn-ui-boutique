064d1d6f60c8b7136bb5a3da41ba47d2639f22ef
a7med3lii <ta.el.ahmed3li@gmail.com>
2025-07-06 22:40:16 +0000

    # Updated Instagram links in Navbar

---
+++ src/data/brands.ts
@@ -0,0 +1,25 @@
+import { Brand } from "@/types";
+
+export const brands: Brand[] = [
+  {
+    id: "classic.color.shop",
+    name: "classic.color.shop",
+    description: "ملابس كلاسيكية أنيقة تناسب جميع المناسبات",
+    instagramLink: "https://www.instagram.com/classic.color.shop",
+    image: "/assets/brands/classic.color.shop.jpg"
+  },
+  {
+    id: "ghyma68",
+    name: "ghyma68",
+    description: "تصاميم عصرية تجمع بين الأناقة والراحة",
+    instagramLink: "https://www.instagram.com/ghyma68",
+    image: "/assets/brands/ghyma68.jpg"
+  },
+  {
+    id: "designerr.mix",
+    name: "designerr.mix",
+    description: "قطع فاخرة مصممة حسب أحدث صيحات الموضة",
+    instagramLink: "https://www.instagram.com/designerr.mix",
+    image: "/assets/brands/designerr.mix.jpg"
+  }
+];
